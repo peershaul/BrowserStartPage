4bb18e5296d7752bcc8159435ad6b7c41f59fabe
peershaul <peer.shaul1@gmail.com>
2021-06-17 08:12:06 +0300

    Update to the selection engine

--- client/script.js
+++ client/script.js
@@ -1,17 +1,56 @@
 // ====== SELECTION ENGINE ========
 
-let primaries = []
-
+/**
+ * Members of this class will be able to be selected in the selection engine
+ */
 class Selectable {
 
-    constructor(elem, index, primary) {
+    constructor(elem, index) {
         this.elem = elem
-        this.primary = primary
-
-        if (primary) primaries.push(this)
-
+        this.index = index
     }
 
+    static unfocus = (primaries) => {
+        for (let i = 0; i < primaries.length; i++) {
+
+            if (primaries[i].elem.classList.contains('focus')) {
+                primaries[i].elem.classList.remove('focus')
+                if (primaries[i].elem.nodeName == 'INPUT')
+                    primaries[i].elem.blur()
+            }
+
+            if (primaries[i].secondaries.length > 0)
+                for (let j = 0; j < primaries[i].secondaries.length; j++)
+                    if (primaries[i].secondaries[j].elem.classList.contains('focus'))
+                        primaries[i].secondaries[j].elem.classList.remove('focus')
+        }
+    }
+
+    focus = (focused, primaries) => {
+        if (this !== focused)
+            Selectable.unfocus(primaries)
+        focused = this
+        this.elem.classList.add('focus')
+        if (this.elem.nodeName === 'INPUT')
+            this.elem.focus()
+
+        return focused
+    }
+}
+
+
+class PrimarySelectable extends Selectable {
+    constructor(elem, index) {
+        super(elem, index)
+        this.secondaries = []
+    }
+
+    addSecondary = sec => {
+        this.secondaries.push(sec)
+    }
+}
+
+class SecondarySelectable extends Selectable {
 
 }
 
@@ -46,8 +85,37 @@ function getTIndex() {
     return sorted_list
 
 }
+/**
+ * Takes all the tindex element and divides them into primaries and secondaries,
+ * return array of primaries and their secondaries inside those primaries
+ * @param {Array} raw_list 
+ * @returns Array of primaries
+ */
+function genHirarchy(raw_list) {
+    let primaries = []
+    for (let i = 0; i < raw_list.length; i++) {
+        if (Number.isInteger(raw_list[i][1]))
+            primaries.push(new PrimarySelectable(raw_list[i][0], raw_list[i][1]))
 
+        else
+            primaries[primaries.length - 1].addSecondary(
+                new SecondarySelectable(raw_list[i][0], raw_list[i][1])
+            )
+    }
+
+
+    return primaries
+
+}
 
 window.onload = function() {
-    console.log(getTIndex())
+    const primaries = genHirarchy(getTIndex())
+    let focused = primaries[0]
+    let index = 0
+    setInterval(() => {
+        index++
+        if (index == primaries.length) index = 0
+        focused = primaries[index].focus(focused, primaries)
+        console.log(focused.elem)
+    }, 2000)
 }
